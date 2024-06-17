18e4c4278b52f1991a6c201d48c5d61936a8cbff
oereo <dlstpgns0406@gmail.com>
2024-06-17 19:34:25 +0900

    chore: base project settings

---
+++ src/main/java/com/github/prismOverseLab/Monitoring_Boilerplate/MonitoringBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.prismOverseLab.Monitoring_Boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringBoilerplateApplication.class, args);
+	}
+
+}
